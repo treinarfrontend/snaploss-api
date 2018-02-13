7b9dc1bd013ad0546fd940dcd934cd001ab1b8aa
wellingtongeek <wellingtongeek@gmail.com>
2018-02-13 00:58:46 -0200

    Alteração no  arquivo server

--- bin/server.js
+++ bin/server.js
@@ -1,20 +1,54 @@
-const express = require('express');
-const http =  require('http');
-const debug = require('debug');
+'use strict'
+const app = require('../src/app');
+const debug = require('debug')('nodestore:server');
+const http = require('http');
 
-const app = express();
-const router = express.Router();
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
+const server = http.createServer(app);
 
-app.get('/', function(req, res, next){
-    res.status(200).send({
-        title: "API",
-        version: "0.0.1"
-    });
-});
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+console.log('API rodando na porta ' + port);
 
-const server =  http.createServer(app);
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
 
-server.listen('3000', function(){
-    console.log('API Rodando');
-})
+function onError(error) {
+    if (error.syscall != 'listen') {
+        throw error;
+    }
+    const bind = typeof port === 'string' ?
+        'Pipe' + port :
+        'Port' + port;
+    switch (error.code) {
+        case 'EACCESS':
+            console.error(bind + 'requires elevanted privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is alerty in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
