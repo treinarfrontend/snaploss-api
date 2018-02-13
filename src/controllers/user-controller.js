7d743b85e4378d127a9d262177731caf9dd14255
wellingtongeek <wellingtongeek@gmail.com>
2018-02-13 01:50:20 -0200

    Removendo parte email

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -22,7 +22,6 @@ exports.post = async (req, res, next) => {
           email: req.body.email,
           password: md5(req.body.password + global.SALT_KEY)
       });
-      emailService.send(req.body.email, 'Bem vindo ao Snaploss', global.EMAIL_TMPL.replace('{0}', req.body.name))
       res.status(201).send({ message: 'Usuário cadatrado com sucesso!' });
   } catch (e) {
       res.status(500).send({
