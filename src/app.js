2167dc8125c91909b5b166ce377eee802b4ee0c1
wellingtongeek <wellingtongeek@gmail.com>
2018-02-13 01:27:07 -0200

    Configuração inicial do app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
 'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config');
+
+const app = express();
+const router = express.Router();
+
+// Connecta ao banco
+
+// Carregar os Models
+
+// Carregar as rotas
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+module.exports = app;
